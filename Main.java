76eab8caa2dfdcb9b4ce7d4b7908f59cfaf7350f
paritoshtripathi935 <76477608+paritoshtripathi935@users.noreply.github.com>
2021-06-18 10:59:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("DA MOON");
-        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.setScene(new Scene(root, 1024, 720));
         primaryStage.show();
     }
 
