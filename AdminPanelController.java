7ed47fa55eb36370880c19a59826f774e629c1a8
paritoshtripathi935 <76477608+paritoshtripathi935@users.noreply.github.com>
2021-07-11 22:39:17 +0530

    paritosh

---
+++ AdminPanelController.java
@@ -0,0 +1,70 @@
+package ControllerClasses;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class AdminPanelController {
+    @FXML
+    public JFXButton UMSbutton;
+    @FXML
+    public JFXButton IncomBtn;
+    @FXML
+    public JFXButton OutgoingBtn;
+
+
+    @FXML
+    public void UMSOpenAction(ActionEvent evt) throws IOException {
+        Parent AdminBoard = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pageDesigns/UMS/UMS.fxml")));
+        Scene AdminBoardScene = new Scene(AdminBoard);
+        Stage userStage = (Stage) ((Node) evt.getSource()).getScene().getWindow();
+        userStage.setScene(AdminBoardScene);
+        userStage.setTitle("User Management System");
+        userStage.show();
+
+    }
+
+    @FXML
+    public void IncomingOpenAction(ActionEvent evt) throws IOException {
+        Parent AdminBoard = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pageDesigns/Incoming/IncomingMenu.fxml")));
+        Scene AdminBoardScene = new Scene(AdminBoard);
+        Stage userStage = (Stage) ((Node) evt.getSource()).getScene().getWindow();
+        userStage.setScene(AdminBoardScene);
+        userStage.setTitle("Incoming");
+        userStage.show();
+    }
+    @FXML
+    public void OutgoingOpenAction(ActionEvent evt) throws IOException {
+        Parent AdminBoard = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pageDesigns/Outgoing/Outgoing.fxml")));
+        Scene AdminBoardScene = new Scene(AdminBoard);
+        Stage userStage = (Stage) ((Node) evt.getSource()).getScene().getWindow();
+        userStage.setScene(AdminBoardScene);
+        userStage.setTitle("Incoming");
+        userStage.show();
+    }
+        @FXML
+        LineChart<String,Number> lineChart;
+        public void loadChart(javafx.event.ActionEvent event){
+            XYChart.Series<String,Number> series = new XYChart.Series<String,Number>();
+            series.getData().add(new XYChart.Data<String,Number>("Jan",200));
+            series.getData().add(new XYChart.Data<String,Number>("Feb",100));
+            series.getData().add(new XYChart.Data<String,Number>("March",500));
+            series.getData().add(new XYChart.Data<String,Number>("April",400));
+            lineChart.getData().add(series);
+        }
+
+
+    }
